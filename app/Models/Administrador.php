7e22d4b299b597942af807fda887fba6d17a7590
blacrumar <blacrumar@alum.us.es>
2023-05-16 00:57:55 +0200

    usuarios

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -4,15 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use 
 
 class Administrador extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
 
-    protected $fillable = ['nombre', 'apellidos', 'DNI', 'telefono', 'email'];
+    //protected $fillable = ['nombre', 'apellidos', 'DNI', 'telefono', 'email'];
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function pedidos(){
+        return $this->hasMany(Pedido::class);
     }
 }
